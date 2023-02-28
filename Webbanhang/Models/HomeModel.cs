479c08d0a159a892b0d86bc1b4a4b34c625c8a90
HUUKHANH <kevinkhanh0701@gmail.com>
2023-02-28 23:04:54 +0700

    cắt giao diện

---
+++ Webbanhang/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Webbanhang.Context;
+namespace Webbanhang.Models
+{
+    public class HomeModel
+    {
+        public List<LoaiSanPham> ListLoaiSanPham { get; set; }
+        public List<SanPham> ListSanPham { get; set; }
+    }
+}
